01bad56e72fb64bb71de987f1fb579004b105686
Jitendra Singh Tomar <jitendrastomar5593@gmail.com>
2024-07-27 21:52:49 +0530

    Add files via upload

---
+++ batch4/day4/DIGITALLY SIGNED.ps1
@@ -0,0 +1,37 @@
+﻿<# DIGITALLY SIGNED SCRIPT
+
+    1. a script (with any code in it)
+    2. a certificate (self signed certificate)
+        - makecert.exe
+        - linux --> openssl
+        - IIS web server
+        - PowerShell cmdlet -=> New-SelfSignedCertificate (PSv5.0)
+    3. export it certificate
+    4. import/install the certificate
+    5. bind the certificate with the script (Set-AuthenticodeSignature)
+    6. verify (Get-AuthenticodeSignature)
+
+#>
+# for creating certificate
+New-SelfSignedCertificate -CertStoreLocation Cert:\currentuser\My `
+-Subject "CN=ltib4" `
+-KeyAlgorithm RSA `
+-KeyLength 1024 `
+-Provider "Microsoft Enhanced RSA and AES Cryptographic Provider" `
+-KeyExportPolicy Exportable `
+-KeyUsage DigitalSignature `
+-Type CodeSigningCert
+
+# to list/view certificate
+Get-ChildItem Cert:\currentuser\My                                               # show all certs
+Get-ChildItem Cert:\currentuser\My\684B3CAB039B5B46BE841027BFF3EC24C1506CC8      # show ur cert
+
+#to export the cert
+get-help Export-PfxCertificate -Online
+
+# to import/install the exported certificate
+Get-Help Import-PfxCertificate -Online
+
+# to bind the script & the cert
+$cert = Get-ChildItem Cert:\CurrentUser\My\684B3CAB039B5B46BE841027BFF3EC24C1506CC8
+Set-AuthenticodeSignature -Certificate $cert -FilePath "E:\PowerShell\batch4\myscript.ps1"
---
+++ batch4/day4/ExecutionPolicy.ps1
@@ -0,0 +1,34 @@
+﻿<#
+    
+    Execution Policy
+        - a policy that allows a user to run or not to run a script on PS.
+        - cmds:
+            1. Get-ExecutionPolicy
+                - list the current policy
+            2. Set-ExecutionPolicy <>
+                - change the policy 
+
+
+    Types of ExecutionPolicy
+        1. Unrestricted
+            - if this policy is active, it will allow users to run ANY/ALL scripts.
+
+        2. Restricted
+            - if this policy is active, it will NOT allow users to run ANY scripts.
+
+        3. RemoteSigned
+            - if this policy is active, local users does not have to digitally sign the script 
+              but remote users script must be DIGITALLY SIGNED.  
+
+        4. AllSigned
+            - both local and remote users must have a DIGITALLY SIGNED SCRIPT.
+
+        5. Default
+        6. ByPass
+
+
+#>
+Get-ExecutionPolicy
+
+Set-ExecutionPolicy RemoteSigned
+Get-ExecutionPolicy
